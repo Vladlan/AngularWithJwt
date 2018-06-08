617dbad859d9cc2999f5d7ba616c911498e1565f
Vladlan <lanvlad911@gmail.com>
2018-06-08 22:13:40 +0300

    provided request logger midleware called morgan

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const jwtService = require('./jwt.service');
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
 
 let faker = require('faker');
 
@@ -15,20 +24,20 @@ const user = {
   id: 'uuidV1'
 };
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send(`Hello World!`);
 });
 
-app.get('/random-user', function (req, res) {
+app.get('/random-user', function(req, res) {
   let user = faker.helpers.createCard();
   res.json(user);
 });
 
-app.get('/check', function (req, res) {
+app.get('/check', function(req, res) {
   let userToken = req.get('authorization');
   userToken = userToken.slice(7);
 
-  if ( jwtService.verifyAndDecodeJWT(userToken) ) {
+  if (jwtService.verifyAndDecodeJWT(userToken)) {
     //Maybe later user will be replaced with db
     res.send(jwtService.verifyAndDecodeJWT(userToken).id === user.id);
   } else {
@@ -36,7 +45,7 @@ app.get('/check', function (req, res) {
   }
 });
 
-app.post('/login', authenticate, function (req, res) {
+app.post('/login', authenticate, function(req, res) {
   if (req.body) {
     res.send(
       {
@@ -46,7 +55,7 @@ app.post('/login', authenticate, function (req, res) {
   }
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
 
